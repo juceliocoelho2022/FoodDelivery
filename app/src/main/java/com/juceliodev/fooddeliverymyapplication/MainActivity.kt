9ec3d49cdf99715ca6a4a0ffaa1a8dd0975b2ffd
Jucelio Coelho <juceliocoelho2010@gmail.com>
2024-09-23 15:28:16 -0300

    MainActivity  OK

--- app/src/main/java/com/juceliodev/fooddeliverymyapplication/MainActivity.kt
+++ app/src/main/java/com/juceliodev/fooddeliverymyapplication/MainActivity.kt
@@ -4,44 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.juceliodev.fooddeliverymyapplication.ui.theme.FoodDeliveryMyApplicationTheme
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.juceliodev.fooddeliverymyapplication.data.OrderState
+import com.juceliodev.fooddeliverymyapplication.ui.screen.ProductDetailsScreen
+import com.juceliodev.fooddeliverymyapplication.ui.theme.AppTheme
+
+
+private const val PRODUCT_PRICE_PER_UNIT = 5.25
+private const val PRODUCT_CURRENCY = "$"
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            FoodDeliveryMyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            AppTheme {
+                var amount by remember { mutableIntStateOf(5) }
+                val totalPrice by remember { derivedStateOf { amount * PRODUCT_PRICE_PER_UNIT } }
+
+                ProductDetailsScreen(
+                    orderState = OrderState(
+                        amount = amount,
+                        totalPrice = "$PRODUCT_CURRENCY${totalPrice}"
+                    ),
+                    onAddItemClicked = { amount = amount.inc() },
+                    onRemoveItemClicked = { if (amount > 0) amount = amount.dec() }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FoodDeliveryMyApplicationTheme {
-        Greeting("Android")
     }
 }
